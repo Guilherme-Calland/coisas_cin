ed57089948380ba4190c4f1941440c9fe90c3e57
guilhermow <gcrb@cin.ufpe.br>
2019-10-31 11:14:30 -0300

    I've heard it too many times to ignore it

--- PLC/threads/Threads/src/cave_6_countdownLatches/AppTeste.java
+++ PLC/threads/Threads/src/cave_6_countdownLatches/AppTeste.java
@@ -1,7 +1,46 @@
 package cave_6_countdownLatches;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class ProcessorTeste implements Runnable{
+	private CountDownLatch l;
+	private int id;
+	public ProcessorTeste(int id, CountDownLatch l){
+		this.id = id;
+		this.l = l;
+	}
+	public void run() {
+		System.out.println("starting " + id);
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("done " + id);
+		l.countDown();
+	}
+}
+
 public class AppTeste {
 	public static void main(String[]args) {
+		CountDownLatch latch = new CountDownLatch(10);
 		
+		ExecutorService pool = Executors.newFixedThreadPool(3);
+		
+		for(int i = 0; i < 10; i++) {
+			pool.submit(new ProcessorTeste(i, latch));
+		}
+		
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("all done");
 	}
 }
