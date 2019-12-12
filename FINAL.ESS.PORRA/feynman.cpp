90512a95dc3eded97c55a5836dc96c1b20040146
Guilherme Calland Rosa Borba <gcrb@ug5c20.windows.cin.ufpe.br>
2019-12-12 14:45:51 -0300

    the lovers the dreamers and me

---
+++ FINAL.ESS.PORRA/feynman.cpp
@@ -0,0 +1,41 @@
+/*
+AGILE:
+user stories: cenarios para elicitar requisitos funcionais
+UI storyboards : elicitar requisitos de User Interface
+Pontos: para estimar o custo de tempo relacionado à implementacao de cada cenário.
+Velocidade: para medir e estimar a velocidade de trebalho de equipe
+cucumber: programa usado para transformar user stories em testes de aceitacao.
+usar programas como pivotal tracker/zenhub ou até features do prórpio github para
+manter registro do progresso, agendar prazos e alocar tarefas aos membros da equipe.
+
+Agile, BDD:
+é um dos pilares da abordagem agile a consulta frequente com os stakeholders.
+É como com as equipes agile se reuniremo (desenvolvedores e stakeholders) de duas em duas semanas
+para crirar novos cenários e checar com os stakeholder que o que foi criado até então
+está como eles imaginaram(validacao).
+
+a cada iteracao (chamada de sprint) os stakeholders são consultudos sobre quais novas features eles queem mplementados
+e tambem para verificar se as features ja presentes se comportam como
+eles esperavam.
+
+o ciclo de vida agile começa tem como base fazer perguntas sobre o comportamento do sistema, antes e durante o seu desenvolvimento
+ao stakeholder para ter certeza se o que ele pediu está de fato sendo implementado.
+
+Como escrever uma user story:
+As a [stakeholder]
+So that I can [acheive some goal]
+I want to [do some task]
+
+Feature: add a comment
+    As a sistem user
+    So that others can see my comment and repond to it
+    I want to write a comment on a post
+
+os cenários são passados para os desenvolvedores com cada desenvolvedor  
+atribuindo uma quantia de pontos a sua user story afim de estimar o quanto
+tempo levaria para implementar-la e e assim ajudar a estimar um porazo para
+a conculsao do projeto.
+
+como fazer uma boa user story: suar o acronimo SMART
+Specific,Measurable,Relevant,Timeboxed
+*/
